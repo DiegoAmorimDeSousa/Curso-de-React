aab46045c2aedf272954d0049bdcde6422c000d6
DiegoDevol <diegoASsousa@gmail.com>
2023-01-11 10:50:21 -0300

    Aula 04

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1, 2)}</p>
       <img src={url} />
+      <HelloWorld />
     </div>
   );
 }
